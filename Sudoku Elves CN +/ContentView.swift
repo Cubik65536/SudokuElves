78d2dc0f4748849ce0380ef6a40c20edf90d6dc9
Cubik65536 <cubik65536@outlook.com>
2021-07-13 10:14:39 -0400

    Add Basic Project Files for iPhone and iPad Versions

---
+++ Sudoku Elves CN +/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sudoku Elves CN +
+//
+//  Created by Cubik65536 on 2021-07-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
