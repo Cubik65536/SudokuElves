9a92a1655ea5c676b973a4f8d38e7b034b5a3b70
Cubik65536 <cubik65536@outlook.com>
2021-07-13 21:43:48 -0400

    Add Interstitial Ads class for iPhone CN

---
+++ Sudoku Elves CN/InterstitialAds.swift
@@ -0,0 +1,50 @@
+//
+//  InterstitialAds.swift
+//  Sudoku Elves
+//
+//  Created by Cubik65536 on 2021-07-13.
+//
+
+import GoogleMobileAds
+import UIKit
+
+final class Interstitial:NSObject, GADFullScreenContentDelegate{
+    private var adUnitID = "ca-app-pub-4483819806888754/4125585953"
+    
+    var interstitial: GADInterstitialAd?
+    
+    override init() {
+        super.init()
+        LoadInterstitial()
+    }
+    
+    func LoadInterstitial(){
+        let request = GADRequest()
+        GADInterstitialAd.load(withAdUnitID: adUnitID,
+                               request: request,
+                               completionHandler: { (ad, error) in
+                                if let error = error {
+                                    print("Failed to load interstitial ad with error: \(error.localizedDescription)")
+                                    return
+                                }
+                                self.interstitial = ad
+                                self.interstitial?.fullScreenContentDelegate = self
+                               }
+        )
+    }
+    
+    func showAd(){
+        print("showAds")
+        LoadInterstitial()
+        if let ad = interstitial {
+            let root = UIApplication.shared.windows.first?.rootViewController
+            ad.present(fromRootViewController: root!)
+        } else {
+            print("Ad wasn't ready")
+        }
+    }
+    
+    func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        LoadInterstitial()
+    }
+}
